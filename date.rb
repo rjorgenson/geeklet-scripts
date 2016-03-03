0d43c28a98a5efba5a4b8603bf845e341e5ccfcd
rjorgenson <rjorgenson@gmail.com>
2016-03-02 23:10:29 -0700

    updated help text for date.rb

--- date.rb
+++ date.rb
@@ -4,8 +4,8 @@ require 'optparse'
 options = {}
 
 parser = OptionParser.new do |opts|
-  opts.banner = "Usage: date.rb [string type]"
-  
+  opts.banner = "Usage: date.rb [options]"
+
   options[:type] = 'long'
   opts.on( '-t TYPE', '--type TYPE', 'Date string type to output (long, short, time, longDate, shortDate, string)' ) do |type|
     options[:type] = type
@@ -28,27 +28,27 @@ class Datetime
   def long # Tuesday, September 23 11:11 PM
     return Time.now.strftime("%A, %B %d %I:%M %p")
   end # def long
-  
+
   def short # Tue, Sep 23 11:11 PM
     return Time.now.strftime("%a, %b %d %I:%M %p")
   end # def short
-  
+
   def time # 11:11 PM
       #    future support for time zones and possibly world clocks!
 #    ENV['TZ']='America/Chicago'
     return Time.now.strftime("%I:%M %p")
   end # def time
-  
+
   def longDate # Tuesday, September 23
     return Time.now.strftime("%A, %B %d")
   end # def longDate
-  
+
   def shortDate # Tue, Sep 23
     return Time.now.strftime("%a, %b %d")
   end # def shortDate
-  
+
   def string(string)# Returns the interpereted value of string
-    return Time.now.strftime(string) 
+    return Time.now.strftime(string)
   end # def string
 end # class DateTime
 
